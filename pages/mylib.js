394bd4fbc57558412790bb6234fdf6b5267e071e
rahulkumar28 <coolrahul28@hotmail.com>
2022-03-14 20:35:01 +0530

    v1.0.2.4

--- pages/mylib.js
+++ pages/mylib.js
@@ -20,10 +20,7 @@ export default function MyLib() {
         loadNFTs()
     }, [])
     async function loadNFTs() {
-        const web3Modal = new Web3Modal({
-            network: "mainnet",
-            cacheProvider: true,
-        })
+        const web3Modal = new Web3Modal()
         const connection = await web3Modal.connect()
         const provider = new ethers.providers.Web3Provider(connection)
         const signer = provider.getSigner()
